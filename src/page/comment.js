e0b762d519cc08ad6198fbdac63bb6ec870ab56d
朱健 <zhujian@yowits.com>
2017-06-05 12:56:05 +0800

    no message

--- src/page/comment.js
+++ src/page/comment.js
@@ -3,44 +3,18 @@
  */
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
+import commentHoc from './commentHOC'
 
-export default class Comment extends Component {
+class Comment extends Component {
 
   static propTypes = {
-    json: PropTypes.object
+    json: PropTypes.object,
+    time: PropTypes.string,
+    title: PropTypes.string
   }
 
   static defaultProps = {
     json: {}
-  }
-
-  constructor () {
-    super()
-    this.state = {
-      timeCal: ''
-    }
-  }
-
-  componentWillMount () {
-    this.calTime()
-  }
-
-  componentDidMount () {
-    this.interval = window.setInterval(() => {
-      this.calTime()
-    }, 5000)
-  }
-
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
-  }
-
-  calTime () {
-    let timeCount = (new Date().getTime() - this.props.json.submitTime) / 1000
-    timeCount = timeCount < 60 ? `${Math.ceil(timeCount)}秒` : `${Math.ceil(timeCount / 60)}分钟`
-    this.setState({
-      timeCal: timeCount
-    })
   }
 
   replaceCode (content) {
@@ -54,11 +28,13 @@ export default class Comment extends Component {
           __html: this.replaceCode(this.props.json.message)
         }}/>
         <p style={{textAlign: 'right'}}>
-          <span>{this.props.json.name}</span>
+          <span>{this.props.json.name}，{this.props.title}</span>
           &nbsp;&nbsp;&nbsp;
-          <span>发表于{this.state.timeCal}前</span>
+          <span>发表于{this.props.time}前</span>
         </p>
       </div>
     )
   }
 }
+
+export default commentHoc(Comment)('评论')

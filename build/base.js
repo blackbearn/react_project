6ce50890af99f24c37c6612f723593ff4c7a11f3
BreathlessWay <731005087@qq.com>
2017-06-08 21:40:14 +0800

    no message

--- build/base.js
+++ build/base.js
@@ -2,91 +2,104 @@ const path = require('path');
 const webpack = require('webpack'); //to access built-in plugins
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HappyPack = require('happypack');
+const os = require('os');
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
 
 module.exports = {
-    target: 'web',
-    output: {
-        filename: 'js/[name].[chunkhash:8].min.js',
-        path: path.resolve('./dist'),
-        chunkFilename: "js/[name].[chunkhash:8].min.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                enforce: 'pre',  // 在babel-loader对源码进行编译前进行lint的检查
-                exclude: /node_modules/,
-                include: path.resolve('./src'),
-                loader: 'eslint-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        "css-loader",
-                        "postcss-loader"
-                    ]
-                })
-            },
-            {
-                test: /\.(js|jsx)$/,
-                use: [
-                    "cache-loader",
-                    "babel-loader"
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        "css-loader",
-                        "postcss-loader",
-                        "less-loader"
-                    ]
-                })
-            }
-        ]
-    },
-    //定义入口baseUrl
-    context: path.resolve("./"),
-    resolve: {
-        modules: ['node_modules', path.resolve('./node_modules')],
-        extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.ts', '.tsx', '.js', '.jsx', '.json', '.css', '.less'],
-        alias: {
-            '@': path.resolve('./src')
-        }
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'js/vendor.[hash].min.js',
-            minChunks: function (module) {
-                // 该配置假定你引入的 vendor 存在于 node_modules 目录中
-                return module.context && module.context.indexOf('node_modules') !== -1;
-            }
-        }),
-        //拷贝文件
-        // new CopyWebpackPlugin([{
-        //     from: './iconfont/iconfont.*',
-        //     to: './',
-        // }]),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false
-        }),
-        //banner
-        new webpack.BannerPlugin({
-            banner: 'Author:BreathlessWay',
-            raw: false,
-            entryOnly: true
-        }),
-        new ExtractTextPlugin({
-            filename: "css/style.[contenthash].css",
-            disable: false,
-            allChunks: true
+  target: 'web',
+  output: {
+    filename: 'js/[name].[chunkhash:8].min.js',
+    path: path.resolve('./dist'),
+    chunkFilename: 'js/[name].[chunkhash:8].min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',  // 在babel-loader对源码进行编译前进行lint的检查
+        exclude: /node_modules/,
+        include: path.resolve('./src'),
+        loader: 'happypack/loader?id=eslint'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'happypack/loader?id=css'
         })
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'happypack/loader?id=js'
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'happypack/loader?id=less'
+        })
+      }
     ]
+  },
+  //定义入口baseUrl
+  context: path.resolve('./'),
+  resolve: {
+    modules: ['node_modules', path.resolve('./node_modules')],
+    extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.ts', '.tsx', '.js', '.jsx', '.json', '.css', '.less'],
+    alias: {
+      '@': path.resolve('./src')
+    }
+  },
+  plugins: [
+    new HappyPack({
+      id: 'eslint',
+      threadPool: happyThreadPool,
+      loaders: ['eslint-loader']
+    }),
+    new HappyPack({
+      id: 'js',
+      threadPool: happyThreadPool,
+      loaders: ['babel-loader']
+    }),
+    new HappyPack({
+      id: 'less',
+      threadPool: happyThreadPool,
+      loaders: ['css-loader', 'postcss-loader', 'less-loader']
+    }),
+    new HappyPack({
+      id: 'css',
+      threadPool: happyThreadPool,
+      loaders: ['css-loader', 'postcss-loader']
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'js/vendor.[hash].min.js',
+      minChunks: function (module) {
+        // 该配置假定你引入的 vendor 存在于 node_modules 目录中
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    //拷贝文件
+    // new CopyWebpackPlugin([{
+    //     from: './iconfont/iconfont.*',
+    //     to: './',
+    // }]),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    //banner
+    new webpack.BannerPlugin({
+      banner: 'Author:BreathlessWay',
+      raw: false,
+      entryOnly: true
+    }),
+    new ExtractTextPlugin({
+      filename: 'css/style.[contenthash].css',
+      disable: false,
+      allChunks: true
+    })
+  ]
 };
--- build/prod.js
+++ build/prod.js
@@ -5,116 +5,131 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const CommonConfig = require('./base.js');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const os = require('os');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = function () {
-    return Merge(CommonConfig, {
-        entry: {
-            index: ['babel-polyfill', './src/index.js'],
-            // vendors: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-router-redux', 'antd', 'prop-types', 'react-redux', 'react-transition-group', 'moment', 'axios', 'immutable', 'q', 'redux-immutablejs', 'redux-promise', 'redux-form', 'redux-immutable']
+  return Merge(CommonConfig, {
+    entry: {
+      index: ['babel-polyfill', './src/index.js']
+      // vendors: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-router-redux', 'antd', 'prop-types', 'react-redux', 'react-transition-group', 'moment', 'axios', 'immutable', 'q', 'redux-immutablejs', 'redux-promise', 'redux-form', 'redux-immutable']
+    },
+    output: {
+      publicPath: './'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+          include: path.resolve('./src/cssImage'),
+          use: [
+            {
+              loader: 'url-loader',
+              query: {
+                limit: 10000,
+                name: './image/[name].[hash:8].[ext]',
+                publicPath: '../'
+              }
+            },
+            'image-webpack-loader'
+          ]
         },
-        output: {
-            publicPath: './'
+        {
+          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+          include: path.resolve('./src/pageImage'),
+          use: [
+            {
+              loader: 'url-loader',
+              query: {
+                limit: 10000,
+                name: './image/[name].[hash:8].[ext]',
+                publicPath: ' '
+              }
+            },
+            //压缩图片
+            'image-webpack-loader'
+          ]
         },
-        module: {
-            rules: [
-                {
-                    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                    include: path.resolve('./src/cssImage'),
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            query: {
-                                limit: 10000,
-                                name: './image/[name].[hash:8].[ext]',
-                                publicPath: '../'
-                            },
-                        },
-                        "image-webpack-loader"
-                    ]
-                },
-                {
-                    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                    include: path.resolve('./src/pageImage'),
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            query: {
-                                limit: 10000,
-                                name: './image/[name].[hash:8].[ext]',
-                                publicPath: ' '
-                            },
-                        },
-                        "image-webpack-loader"
-                    ]
-                },
-                {
-                    test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
-                    loader: 'url-loader',
-                    query: {
-                        limit: 10000,
-                        name: './iconfont/[name].[hash:7].[ext]',
-                        publicPath: '../'
-                    }
-                }
-            ]
+        {
+          test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          query: {
+            limit: 10000,
+            name: './iconfont/[name].[hash:7].[ext]',
+            publicPath: '../'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      //压缩css
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { discardComments: { removeAll: true } },
+        canPrint: true
+      }),
+      //清理dist目录
+      new CleanWebpackPlugin('*', {
+        root: path.resolve('./dist'), // node 的path模块  生成绝对路径
+        verbose: true, // 输出清除信息
+        dry: false // 不清除，测试用
+        // exclude: ["files", "to", "ignore"]
+      }),
+      new ParallelUglifyPlugin({
+        workerCount: os.cpus().length,
+        cacheDir: '.cache/',
+        uglifyJS: {
+          beautify: false,
+          sourceMap: false,
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          },
+          compress: {
+            warnings: false,
+            screw_ie8: true,
+            drop_console: true
+          },
+          comments: false
+        }
+      }),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|jsx|html|css|less)$/,
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        chunks: ['vendor']
+      }),
+      new HtmlWebpackPlugin({
+        title: 'react-project',
+        filename: 'index.html',
+        template: './index.html',
+        inject: true,
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify('production')
-            }),
-            //压缩css
-            new OptimizeCssAssetsPlugin({
-                assetNameRegExp: /\.css$/g,
-                cssProcessor: require('cssnano'),
-                cssProcessorOptions: {discardComments: {removeAll: true}},
-                canPrint: true
-            }),
-            //清理dist目录
-            new CleanWebpackPlugin('*', {
-                root: path.resolve('./dist'), // node 的path模块  生成绝对路径
-                verbose: true, // 输出清除信息
-                dry: false, // 不清除，测试用
-                // exclude: ["files", "to", "ignore"]
-            }),
-            new webpack.optimize.UglifyJsPlugin({
-                beautify: false,
-                sourceMap: false,
-                mangle: {
-                    screw_ie8: true,
-                    keep_fnames: true
-                },
-                compress: {
-                    warnings: false,
-                    screw_ie8: true,
-                    drop_console: true,
-                },
-                comments: false
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: "manifest",
-                chunks: ['vendor']
-            }),
-            new HtmlWebpackPlugin({
-                title: 'react-project',
-                filename: 'index.html',
-                template: './index.html',
-                inject: true,
-                minify: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    keepClosingSlash: true,
-                    minifyJS: true,
-                    minifyCSS: true,
-                    minifyURLs: true,
-                },
-                favicon: '',
-                hash: false,
-                showErrors: true
-            })
-        ]
-    });
+        favicon: '',
+        hash: false,
+        showErrors: true
+      })
+    ]
+  });
 };
